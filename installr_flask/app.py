f3978ce6c80b177efffedbb533bdf4ee014f5465
Uday Singh <udaysingh.code@gmail.com>
2015-05-02 22:12:28 -0400

    completed backend

---
+++ installr_flask/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/i/<url>')
+def url_return_install(url):
+  returnValue = ""
+  if (url.find("%") != -1):
+    # do stuff to pull it out
+    wordList = url.split("%")
+    for word in wordList:
+      returnValue += 'brew_cask_install "' + word + '"<br/>'
+  else:
+    returnValue += 'brew_cask_install "' + url + '"'
+  return returnValue
+ 
+@app.route('/u/<url>')
+def url_return_uninstall(url):
+  returnValue = ""
+  if (url.find("%") != -1):
+    # do stuff to pull it out
+    wordList = url.split("%")
+    for word in wordList:
+      returnValue += 'brew_cask_uninstall "' + word + '"<br/>'
+  else:
+    returnValue += 'brew_cask_uninstall "' + url + '"'
+  return returnValue
+ 
+if __name__ == '__main__':
+    app.run()
